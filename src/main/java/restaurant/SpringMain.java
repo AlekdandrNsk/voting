c4b095a1be192b5e4a51a2b65a4e4d92013a506d
AlekdandrNsk <aleksparshukov@yandex.ru>
2018-05-28 03:14:02 +0700

    spring context

---
+++ src/main/java/restaurant/SpringMain.java
@@ -0,0 +1,23 @@
+package restaurant;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+import java.util.Arrays;
+
+public class SpringMain {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+
+//        UserRepository userRepository = (UserRepository) appCtx.getBean("mockUserRepository");
+//        UserRepository userRepository = appCtx.getBean(UserRepository.class);
+//        userRepository.getAll();
+//
+//        UserService userService = appCtx.getBean(UserService.class);
+//        userService.create(new User(null, "userName", "email", "password", Role.ROLE_ADMIN));
+
+        appCtx.close();
+    }
+}
--- src/main/java/restaurant/web/RestaurantServlet.java
+++ src/main/java/restaurant/web/RestaurantServlet.java
@@ -1,8 +1,11 @@
 package restaurant.web;
 
 import org.slf4j.Logger;
-import restaurant.util.DishUtil;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import restaurant.repository.DishRepository;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,11 +15,30 @@ import java.io.IOException;
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class RestaurantServlet extends HttpServlet {
+
     private static final Logger log = getLogger(RestaurantServlet.class);
+
+    private ConfigurableApplicationContext springContext;
+    private DishRepository repository;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        springContext = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        repository = springContext.getBean(DishRepository.class);
+    }
+
+    @Override
+    public void destroy() {
+        springContext.close();
+        super.destroy();
+    }
+
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("forward to restaurants");
 
-        request.setAttribute("restaurants",DishUtil.MENU);
+        request.setAttribute("restaurants", repository.getMENU());
         request.getRequestDispatcher("/restaurants.jsp").forward(request, response);
     }
 }
