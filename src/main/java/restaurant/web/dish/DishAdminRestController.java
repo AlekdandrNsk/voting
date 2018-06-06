ccbae0e3296275c24f73e23c8bab57f32b422cb9
AlekdandrNsk <aleksparshukov@yandex.ru>
2018-06-07 06:23:17 +0700

    controllers1

---
+++ src/main/java/restaurant/web/dish/DishAdminRestController.java
@@ -0,0 +1,66 @@
+package restaurant.web.dish;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import restaurant.AuthorizedUser;
+import restaurant.model.Dish;
+import restaurant.model.Meal;
+import restaurant.model.User;
+import restaurant.model.Vote;
+import restaurant.repository.datajpa.DishRepository;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.time.LocalDate;
+import java.util.List;
+
+import static restaurant.util.UserUtil.prepareToSave;
+
+@RestController
+@RequestMapping("/rest/dishes")
+public class DishAdminRestController {
+
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private DishRepository repository;
+
+    // create new vote
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Dish> createWithLocation(@RequestBody Dish dish) {
+        repository.save(dish);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/rest/votes" + "/{id}")
+                .buildAndExpand(dish.getId()).toUri();
+        return ResponseEntity.created(uriOfNewResource).body(dish);
+    }
+
+
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@RequestBody Dish dish, @PathVariable("id") int id) {
+        repository.save(dish);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") int id) {
+        int userId = AuthorizedUser.id();
+        log.info("delete dish {} for user {}", id, userId);
+        repository.delete(id);
+    }
+
+    @GetMapping("/{id}")
+    public Dish get(@PathVariable("id") int id) {
+        int userId = AuthorizedUser.id();
+        log.info("get dish {} for user {}", id, userId);
+        return repository.findById(id).orElse(null);
+    }
+
+}
