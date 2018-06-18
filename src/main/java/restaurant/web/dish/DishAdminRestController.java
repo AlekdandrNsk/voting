27c1a5f80f3be89d6cd11e85db159ba444165cbd
AlekdandrNsk <aleksparshukov@yandex.ru>
2018-06-18 17:56:48 +0700

    fix Dish ctrlr

--- src/main/java/restaurant/web/dish/DishAdminRestController.java
+++ src/main/java/restaurant/web/dish/DishAdminRestController.java
@@ -1,5 +1,8 @@
 package restaurant.web.dish;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -8,20 +11,32 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import restaurant.model.Dish;
+import restaurant.repository.datajpa.DishRepository;
 
 import javax.validation.Valid;
 import java.net.URI;
 
+import static restaurant.util.ValidationUtil.assureIdConsistent;
+import static restaurant.util.ValidationUtil.checkNew;
+import static restaurant.util.ValidationUtil.checkNotFoundWithId;
+
 @RestController
 @RequestMapping(DishAdminRestController.REST_URL)
-public class DishAdminRestController extends AbstractDishController {
+public class DishAdminRestController{
     static final String REST_URL = "/rest/admin/dishes";
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private DishRepository repository;
 
     @CacheEvict(value = "dishes", allEntries = true)
     @Transactional
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Dish> createWithLocation(@Valid @RequestBody Dish dish) {
-        super.save(dish);
+        log.info("create {}", dish);
+        checkNew(dish);
+        repository.save(dish);
         URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
                 .path("/rest/votes" + "/{id}")
                 .buildAndExpand(dish.getId()).toUri();
@@ -33,19 +48,23 @@ public class DishAdminRestController extends AbstractDishController {
     @Transactional
     @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void update(@Valid @RequestBody Dish dish, @PathVariable("id") int id) {
-        super.update(dish, id);
+        log.info("update {} with id={}", dish, id);
+        assureIdConsistent(dish, id);
+        checkNotFoundWithId(repository.save(dish), id);
     }
 
     @CacheEvict(value = "dishes", allEntries = true)
     @DeleteMapping("/{id}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void delete(@PathVariable("id") int id) {
-        super.delete(id);
+        log.info("delete dish {}", id);
+        checkNotFoundWithId(repository.delete(id), id);
     }
 
     @GetMapping("/{id}")
     public Dish get(@PathVariable("id") int id) {
-        return super.get(id);
+        log.info("get dish {}", id);
+        return checkNotFoundWithId(repository.findById(id).orElse(null), id);
     }
 
 }
--- src/main/java/restaurant/web/dish/DishRestController.java
+++ src/main/java/restaurant/web/dish/DishRestController.java
@@ -1,5 +1,8 @@
 package restaurant.web.dish;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import restaurant.model.Dish;
+import restaurant.repository.datajpa.DishRepository;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -15,12 +19,19 @@ import static restaurant.web.dish.DishRestController.REST_URL;
 
 @RestController
 @RequestMapping(REST_URL)
-public class DishRestController extends AbstractDishController {
+public class DishRestController{
     static final String REST_URL = "/rest/dishes";
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private DishRepository repository;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Dish> getAllByDay(@RequestParam(value = "date", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
-        return super.getAllByDay(date);
+        LocalDate requiredDate = (date == null ? LocalDate.now() : date);
+        log.info("getAll dishes by {}", requiredDate);
+        return repository.findAllByDate(requiredDate);
     }
 
 }
